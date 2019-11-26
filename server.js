4d3501c70f4e02e82cf5cff12f09357d120e9a66
MALYP001 <pablo.x.malynovitch.-nd@disney.com>
2019-11-26 14:16:05 -0300

    Redo App

--- server.js
+++ server.js
@@ -1,30 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const Account = require('./public/functions').instance;
 var fs = require('fs');
 
 app.use(cors());
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    var filePath = 'db.json';
+app.get('/', async function(req, res, next) {
+    try {
+        const history = await Account.getTransactionHistory();
     
-    if (fs.existsSync(filePath)) {
-        // load file
-        try {
-            content = fs.readFileSync(filePath, 'utf-8');
-            data = JSON.parse(content)
-            deferred.resolve(data);
-        } catch (err) {
-            console.log('Example app listening on port 3000!')
-        }
-    } else {
-          deferred.reject();
-    }
-    res.status(200);
-    res.json(data);
+        res.status(200).send(history);
+      } catch (e) {
+        console.error(e);
+        res.status(400).send({ message: 'invalid status value' });    
+      }
 });
+
+app.post('/transaction', async function(req, res, next) {
+    try {
+      const transaction = await Account.commitTransaction(req.headers);
+  
+      res.status(200).send(transaction);
+    } catch (e) {
+      console.error(e);
+      res.status(403).send({ message: 'transaction refused' });    
+    }
+  });
+
+  app.get('/transaction/:id/', async function(req, res, next) {
+    try {
+      const transaction = await Account.getTransactionById(req.params.id);
+      if (transaction) {
+        return res.status(200).send(transaction);
+      }
+  
+      res.status(404).send({ message: 'transaction not found' });
+    } catch (e) {
+      console.error(e);
+      res.status(422).send({ message: 'invalid ID supplied' });
+    }
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
